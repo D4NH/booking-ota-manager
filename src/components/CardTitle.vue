fffcd1551338e3cfce0d92dee986d5e6394a926f
Yan Nguyen <nguyen.dtd@gmail.com>
2026-09-14 16:24:23 +0700

    added cardtitle and pagetitle components

---
+++ src/components/CardTitle.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="shrink-0 mt-4 mb-4">
+        <h2 class="text-sm font-bold uppercase tracking-wider text-mist-100">
+            <slot name="title">Title</slot>
+        </h2>
+        <p class="mt-1 text-xs text-mist-500"><slot name="subtitle">Subtitle</slot></p>
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { getCurrentDate, formatDate } from '@/utils/date';
+</script>
+
+<template>
+    <div class="flex shrink-0 items-start justify-between mt-4">
+        <div>
+            <h1 class="text-xl font-bold text-mist-100"><slot name="title">Title</slot></h1>
+            <div class="mt-1 text-xs text-mist-400">
+                <slot name="subtitle">Subtitle</slot>
+            </div>
+        </div>
+        <div class="text-xs text-mist-500">
+            {{ formatDate(getCurrentDate(), { includeYear: true }) }}
+        </div>
+    </div>
+</template>
