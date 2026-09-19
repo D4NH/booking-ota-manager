ccb7b984c23e0de2a99b1850cc3f8c90a8899558
Yan Nguyen <nguyen.dtd@gmail.com>
2026-09-19 16:24:41 +0700

    added delete item button to personalFinanceTable

--- src/components/finance/PersonalFinanceTable.vue
+++ src/components/finance/PersonalFinanceTable.vue
@@ -22,6 +22,7 @@ const {
     addSharedTransaction,
     editPersonalTransaction,
     editSharedTransaction,
+    removePersonalTransaction,
 } = useFinanceSync();
 const { filteredPersonalFinances, filteredSharedFinances, isLoading, currentGoldPricePerGram } =
     storeToRefs(financeStore);
@@ -309,24 +310,27 @@ watch(formCategory, (newCat) => {
                             {{ item.type === 'income' ? '+' : '-' }}{{ formatIDR(item.amount) }}
                         </td>
                         <td class="py-3 px-3 text-center">
-                            <button
-                                type="button"
-                                title="Edit Transaction"
-                                class="opacity-70 group-hover:opacity-100 text-mist-400 hover:text-lime-400 p-1 rounded hover:bg-mist-800 transition"
-                                @click="openEditModal(item)">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="w-3.5 h-3.5"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-                                    <path d="m15 5 4 4" />
-                                </svg>
-                            </button>
+                            <div class="flex items-center justify-end gap-1">
+                                <button
+                                    type="button"
+                                    title="Edit Transaction"
+                                    class="opacity-70 group-hover:opacity-100 text-mist-400 hover:text-lime-400 p-1 rounded hover:bg-mist-800 transition"
+                                    @click="openEditModal(item)">
+                                    <fa-icon
+                                        icon="pen-to-square"
+                                        class="text-xs" />
+                                </button>
+                                <span class="text-mist-700">|</span>
+                                <button
+                                    type="button"
+                                    title="Delete Transaction"
+                                    class="opacity-70 group-hover:opacity-100 text-mist-400 hover:text-rose-400 p-1 rounded hover:bg-mist-800 transition"
+                                    @click="removePersonalTransaction(item.id, item.category)">
+                                    <fa-icon
+                                        icon="trash-can"
+                                        class="text-xs" />
+                                </button>
+                            </div>
                         </td>
                     </tr>
                     <tr v-if="currentList.length === 0">
